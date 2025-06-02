4576618f2343d31661adc5fd932a702bc59ccbf8
Wolfgang Fahl <wf@bitplan.com>
2025-06-02 14:29:07 +0200

    prepares release and installs libreoffice in github CI

--- slides/version.py
+++ slides/version.py
@@ -19,12 +19,12 @@ class Version:
     description = "generate Semantic Mediawiki for a set of powerpoint presentations with semantic annotations"
     version = slides.__version__
     date = "2023-02-14"
-    updated = "2023-02-22"
+    updated = "2025-06-02"
     authors = "Wolfgang Fahl"
     doc_url = "https://wiki.bitplan.com/index.php/PySemanticSlides"
     chat_url = "https://github.com/WolfgangFahl/pySemanticSlides/discussions"
     cm_url = "https://github.com/WolfgangFahl/pySemanticSlides"
-    license = f"""Copyright 2020-2023 contributors. All rights reserved.
+    license = f"""Copyright 2020-2025 contributors. All rights reserved.
   Licensed under the Apache License 2.0
   http://www.apache.org/licenses/LICENSE-2.0
   Distributed on an "AS IS" basis without warranties
